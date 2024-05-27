d15d285436c2045d32aa8520ed8198903b277f74
nikolvett <20418@uktc-bg.com>
2024-05-27 18:57:03 +0300

    Update roles.sql

--- roles.sql
+++ roles.sql
@@ -1,18 +1,13 @@
--- Create roles and assign privileges
 CREATE ROLE db_manager;
 CREATE ROLE table_viewer;
 CREATE ROLE column_modifier;
 
--- Database level privilege
 GRANT CONTROL ON DATABASE::JewelryShop TO db_manager;
 
--- Table level privilege
 GRANT SELECT ON OBJECT::Products TO table_viewer;
 
--- Column level privilege
 GRANT UPDATE (Price) ON OBJECT::Products TO column_modifier;
 
--- Create users and assign roles
 CREATE USER db_manager_user FOR LOGIN db_manager_user;
 CREATE USER table_viewer_user FOR LOGIN table_viewer_user;
 CREATE USER column_modifier_user FOR LOGIN column_modifier_user;
